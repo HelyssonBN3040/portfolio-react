a439f6955dccf79e12c179c0fa0ab9515861cb8b
HelyssonBN3040 <helyssoncavalcante.com.@gmail.com>
2024-02-23 13:18:19 -0300

    First commit

---
+++ src/components/ButtonCV/ButtonCv.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ButtonCv.css"
+
+const ButtonCv = () => {
+  return (
+    <div>
+        <a className='ButtonCv' href="https://drive.google.com/file/d/1Qxi5ekINuGq6UhctYHARPyMJEqIzQr-0/view?usp=sharing">Download CV</a>
+    </div>
+  )
+}
+
+export default ButtonCv
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ButtonCv from '../ButtonCV/ButtonCv'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div>
+        <header className='ContainerNav'>
+            <ul className="NavBar">
+                <li>Home</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+                <li>Certificados</li>
+            </ul>
+            <ButtonCv/>
+        </header>
+    </div>
+  )
+}
+
+export default NavBar
